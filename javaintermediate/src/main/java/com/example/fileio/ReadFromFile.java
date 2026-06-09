b1b2dab03c6b9a27722dc1f2928c8cc7e3ed0853
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-06-09 13:34:08 +0530

    add filereader

---
+++ javaintermediate/src/main/java/com/example/fileio/ReadFromFile.java
@@ -0,0 +1,30 @@
+package com.example.fileio;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFromFile {
+    public static void main(String[] args) {
+        // reach one character at a time
+        // FileReader
+        FileReader fr = null;
+        try {
+            fr = new FileReader("myfile.txt");
+            int ch;
+
+            while ((ch = fr.read()) != -1) {
+                System.out.print((char) ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
